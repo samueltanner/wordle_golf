ff930e922fca05581f0d03ed0b0550a0b206af3a
samueltanner <samuel.m.s.tanner@gmail.com>
2022-04-23 08:49:27 -0700

    adds heroku to allowed hosts

--- wordle_golf/settings.py
+++ wordle_golf/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = os.environ.get(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = ['.ngrok.io', 'localhost']
+ALLOWED_HOSTS = ['.ngrok.io', 'localhost', '.herokuapp.com']
 
 
 # Application definition
